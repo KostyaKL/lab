7a13f6eff8fce841965c871a0ea10d9ae90dad2a
kostya-laptop <kostya@LENOVO-PC>
2016-11-28 22:50:13 +0200

    hagasha 1 comments

--- lab/lab/hagasha 1.c
+++ lab/lab/hagasha 1.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int op1();
-int op2();
-int op3();
-int op4(coutn);
-int op5();
-int other();
+int op1();	//option 1 function
+int op2();	//option 2 function
+int op3();	//option 3 function
+int op4(coutn);	//option 4 function
+int op5();	//option 5 function
+int other();	//any other choise function
 
 int hagasha_1()
 {
 	int num_sel, on = 1, count=0;
+	//num_sel - user coise
+	//on - loop condition
+	//count - how many times the main manue poped
 
 	do
 	{
@@ -45,7 +48,7 @@ int hagasha_1()
 	exit(1);
 }
 
-int op1()
+int op1() //print disconectin and return to main menue
 {
 	system("cls");
 	printf("Disconnecting\n\n");
@@ -53,19 +56,20 @@ int op1()
 	return 1;
 }
 
-int op2()
+int op2()	//return to main menue
 {
 	return 1;
 }
 
-int op3()
+int op3()	//exit only if 'Y' or 'y' is pressed, otherwisse back to main menue
 {
 	char user_c;
+	//user_c - the character that is entered by the user
 	system("cls");
 	printf("Are you sure you want to cancel the order???\n"
 		"Please enter one char: ");
 	scanf(" %c", &user_c);
-	if (user_c == 89 || user_c == 121)
+	if (user_c == 89 || user_c == 121)	//89 - Y	//121 - y
 	{
 		printf("Canceled\n\n");
 		system("pause");
@@ -75,7 +79,7 @@ int op3()
 
 }
 
-int op4(int count)
+int op4(int count)	//show how many times you have visited the main manue
 {
 	system("cls");
 	printf("Your position in queue is %d\n\n", count);
@@ -83,12 +87,12 @@ int op4(int count)
 	return 1;
 }
 
-int op5()
+int op5()	//exit
 {
 	return 0;
 }
 
-int other()
+int other()	//return to main manue when any other number than 1-5 is pressed
 {
 	system("cls");
 	printf("Wrong input\n\n");
