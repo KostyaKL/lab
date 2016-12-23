12f33aede1a70caad999c13303f5e622e79110f6
kostya-laptop <kostya@LENOVO-PC>
2016-12-23 18:26:16 +0200

    hagasha 2 setup

--- lab/lab/hagasha 2.c
+++ lab/lab/hagasha 2.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int h2_ex1();
+int h2_ex2();
+int h2_ex3();
+int h2_ex4();
+int h2_ex5();
+
+
 
 int hagasha_2()
 {
 	int select = 0, i, all_Ex_in_loop = 0;
+	system("cls");
 	printf("Run menu once or cyclically?\n(Once - enter 0, cyclically - enter other number) ");
 		if (scanf("%d", &all_Ex_in_loop) == 1)
 			do
@@ -16,12 +26,47 @@ int hagasha_2()
 					scanf("%d", &select);
 				} while ((select<0) || (select>5));
 				switch (select) {
-				case 1: Ex1(); break;
-				case 2: Ex2(); break;
-				case 3: Ex3(); break;
-				case 4: Ex4(); break;
-				case 5: Ex5(); break;
+				case 1: h2_ex1(); break;
+				case 2: h2_ex2(); break;
+				case 3: h2_ex3(); break;
+				case 4: h2_ex4(); break;
+				case 5: h2_ex5(); break;
 				}
 			} while (all_Ex_in_loop && select);
-			return 0;
+			return main();
+}
+
+///////////////////////////////////////////////////////////////
+
+int h2_ex1()
+{
+	return 1;
+}
+
+///////////////////////////////////////////////////////////////
+
+int h2_ex2()
+{
+	return 1;
+}
+
+///////////////////////////////////////////////////////////////
+
+int h2_ex3()
+{
+	return 1;
+}
+
+///////////////////////////////////////////////////////////////
+
+int h2_ex4()
+{
+	return 1;
+}
+
+///////////////////////////////////////////////////////////////
+
+int h2_ex5()
+{
+	return 1;
 }
